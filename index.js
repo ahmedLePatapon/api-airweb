1eee09fd4131f99376c6e5424465503f9ceb3a18
ahmed <abenlebna@qualycloud.com>
2022-09-06 17:13:59 +0200

    add DB

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const app = express();
+const knex = require('knex');
 
 const PORT = '8080';
 
-app.get('/', (req, res) => {
+const DB = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: "./DATABASE.sqlite"
+    },
+    useNullAsDefault: true
+});
+
+const app = express();
+
+app.get('/', async (req, res) => {
     res.status(200).send('API AIRWEB');
+});
+
+app.get('/catalogue', async (req, res) => {
+    const products = await DB('products').select();
+    res.status(200).json({status: 'OK', products});
 });
 
 app.listen(PORT, () => {
