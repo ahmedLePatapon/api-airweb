ecfa3b262f02203247329b951b2a0fb34fd5ff1e
ahmed <abenlebna@qualycloud.com>
2022-09-07 10:48:07 +0200

    update variables

--- config/variables.js
+++ config/variables.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 const CLIENT_DB = process.env.CLIENT_DB || 'sqlite3';
-const FILE_NAME_DB = process.env.FILE_NAME_DB || "./DATABASE.sqlite";
+const FILE_NAME_DB = process.env.FILE_NAME_DB || "./db.sqlite";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'secret_Acces_Token';
 const TTL_SESSION = process.env.TTL_SESSION * 1 || 60000 ;
 const SALT = process.env.SALT * 1 || 24;
